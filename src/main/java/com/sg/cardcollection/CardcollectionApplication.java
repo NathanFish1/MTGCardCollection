bb5176cf790a67734439944c1387169649b4c9af
NathanFish1 <nathan.fish23@gmail.com>
2023-03-21 12:08:17 +0000

    Eclispe Project
    Added in the start.spring.io file that auto built the spring boot, maven project with jdbc template and mysql connections.
    
    Imported file as a eclipse project. Ready to start editing.

---
+++ src/main/java/com/sg/cardcollection/CardcollectionApplication.java
@@ -0,0 +1,13 @@
+package com.sg.cardcollection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardcollectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardcollectionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sg/cardcollection/CardcollectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sg.cardcollection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardcollectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
